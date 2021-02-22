6beac1042e8f6ca6bc9437be5437128311c34ef6
Jose D'lucas <jsdlcs266@gmail.com>
2021-02-22 02:33:53 -0400

    Cambios subir

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,15 +43,14 @@
                              alt="User Image">
                         <p>
                             {{ Auth::user()->name }}
-                            <small>Member since {{ Auth::user()->created_at->format('M. Y') }}</small>
+                            <small>Registrado desde {{ Auth::user()->created_at->format('M. Y') }}</small>
                         </p>
                     </li>
                     <!-- Menu Footer-->
                     <li class="user-footer">
-                        <a href="#" class="btn btn-default btn-flat">Profile</a>
                         <a href="#" class="btn btn-default btn-flat float-right"
                            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                            Sign out
+                            Salir
                         </a>
                         <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                             @csrf
